c5816c1447f255b8ac5f38f753ceaee2d8c79f99
Pedro Henrique <113939119+PedroVaillant@users.noreply.github.com>
2023-01-30 19:45:25 -0300

    Update sisresilia.sql

--- sisresilia.sql
+++ sisresilia.sql
@@ -1,35 +1,25 @@
--- create database resilia;
+create database resilia;
 
--- use resilia; 
+use resilia; 
 
--- create table alunos (
--- id int primary key auto_increment,
--- CPF varchar(15),
--- nome varchar(40),
--- email varchar(40),
--- telefone varchar(15),
--- endereco varchar(50),
--- n_matricula varchar(40)
--- );
-
--- create table cursos (
--- id int primary key auto_increment,
--- nome varchar(40),
--- descricao varchar(60)
--- );
+create table alunos (
+id int primary key auto_increment,
+cpf varchar(15),
+nome varchar(40),
+email varchar(40),
+telefone varchar(15),
+endereco varchar(50),
+n_matricula varchar(40));
 
 -- create table turmas (
 -- id int primary key auto_increment,
--- n_turma varchar(40),
--- turno varchar(10)
--- );
+n_turma varchar(40),
+turno varchar(10));
 
--- create table professor (
--- id int primary key auto_increment,
--- nome varchar(40),
--- n_matricula varchar(40),
--- email varchar(40),
--- telefone varchar(15),
--- endereco varchar(50)
-
--- );
+create table professor (
+id int primary key auto_increment,
+nome varchar(40),
+n_matricula varchar(40),
+email varchar(40),
+telefone varchar(15),
+endereco varchar(50));
